a35d8bffba2b7e53fcc66e9f0cce9878021f6a95
Emma B <emmabaldachin@kubrickgroup.com>
2019-01-08 09:39:52 +0000

    no message

---
+++ script1.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+'''My Module'''
+
+import numpy as np
+import pandas as pd
+import json
+# then leave 2 lines
+
+def main():
+# this is defining a function, nothing special about the word main but the entry point tends to be main by convention
+	'''
+	This is a docstring, I am learning variables, this is my main function, this comment could span multiple lines
+	Author ME, date 08/01
+	-- 
+    Ammended on: ...
+	Ammended by: ...
+	Issue Ticket No,: 57461 
+	'''
+	myname = 'Emma'
+	print(myname)
+# always a space after hash, best to do comments in their own line
+main()
